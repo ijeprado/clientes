71322dd20135cc219c5b5fda014583450504930d
ijeprado <102391551+ijeprado@users.noreply.github.com>
2026-04-25 17:35:00 -0300

    Tratando erro de usuário duplicado

--- services/auth_service.py
+++ services/auth_service.py
@@ -2,15 +2,24 @@ from fastapi import HTTPException
 
 from models.usuario import Usuario
 from core.security import criar_token, gerar_hash_senha, verificar_senha
+from sqlalchemy.exc import IntegrityError
 
 
 def criar_usuario_service(db, usuario):
-    hash_senha = gerar_hash_senha(usuario.senha)
-    novo = Usuario(username=usuario.username, senha_hash=hash_senha)
-    db.add(novo)
-    db.commit()
+    usuario_existente = db.query(Usuario).filter_by(username=usuario.username).first()
+    if usuario_existente:
+        raise HTTPException(400, "Usuário já existe")
 
-    return {"message": "Usuário criado com sucesso"}
+    try:
+        hash_senha = gerar_hash_senha(usuario.senha)
+        novo = Usuario(username=usuario.username, senha_hash=hash_senha)
+        db.add(novo)
+        db.commit()
+        db.refresh(novo)
+        return novo
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="Username já existe")
 
 
 def login_service(db, dados):
